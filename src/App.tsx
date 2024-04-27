37503d0253476e21288e3e986e44c342641ba11a
Brian Slaby <brian.slaby@gmail.com>
2024-04-26 20:39:32 -0400

    created monsterData file which will eventually destructure data from each separate book into a single array. Added comments to App and tentative state variable.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import { monsterStatBlock } from "../src/types"
 
 
 
 export default function App(): React.JSX.Element {
-  
+  const [ selectedMonsters, setSelectedMonsters ] = useState<monsterStatBlock[]>([])
+
+  // Should App check for edition and render a separate homepage depending on edition state?  Obviously selectedMonsters would get moved to edition pages, and app would contain currentEdition state.
 
   return (
     <>
---
+++ src/data/monsterData.ts
@@ -0,0 +1,10 @@
+import { crbMonsterData } from "./crb-monsters"
+import { monsterStatBlock } from "../types"
+
+const monsterData: monsterStatBlock[] = [
+    ...crbMonsterData
+]
+
+export {
+    monsterData
+}
