6122b7995ad94b4fa8921fa8f43d0d73663d3fdd
Brian Slaby <brian.slaby@gmail.com>
2024-04-27 16:57:42 -0400

    added placeholder intro message to Home, minor styling adjustments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,12 @@ export default function Home(): React.JSX.Element {
     return (
       <div className="home-container">
 
-        <p>content</p>
+        {
+            selectedMonsters.length === 0 ? 
+            <p 
+                className="intro-message" 
+            >You don't have any monsters selected.  To build a combat encounter, click "choose monsters" in the header, select the desired monster levels, and then select a monster from the dropdown.</p> : ""
+        }
        
       </div>
     )
